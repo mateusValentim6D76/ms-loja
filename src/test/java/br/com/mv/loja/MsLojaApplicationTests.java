83381eab27a8ad82679a4a756e3f0070aff33898
Mateus Valentim de Oliveira <mateus.valentim19@hotmail.com>
2023-02-22 09:01:20 -0300

    Criando estrutura do projeto

---
+++ src/test/java/br/com/mv/loja/MsLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mv.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
